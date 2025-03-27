2abde38406e308a42f0f3d5d4f2ab88c91a4e314
pkimpgithub <87598294+pkimpgithub@users.noreply.github.com>
2025-03-27 13:46:51 +0530

    app.py

--- wfh_mail_report_web/app.py
+++ wfh_mail_report_web/app.py
@@ -47,4 +47,4 @@ def download():
     return send_file(os.path.join(UPLOAD_FOLDER, 'email_report.csv'), as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
